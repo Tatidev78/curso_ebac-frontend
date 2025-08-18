0e54363e3a2a57dd95093bac296e29283ea1121c
Tati Dev <tatianaworkhome@gmail.com>
2025-08-18 15:45:07 -0300

    Adicionando funções do exercício TS

---
+++ exercicio_ts/exercicio_ts.js
@@ -0,0 +1,8 @@
+function multiplicar(a, b) {
+    return a * b;
+}
+function cumprimentar(nome) {
+    return " Olá" + nome;
+}
+console.log(multiplicar(11, 78));
+console.log(cumprimentar("Tatiana"));
---
+++ exercicio_ts/exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b:number) : number {
+    return a * b;
+}
+
+function cumprimentar(nome: string) : string{
+    return `Olá ${nome}`;    
+}
+
+console.log(multiplicar(11,78));
+console.log(cumprimentar("Tatiana"));
